e26939678fc0fecce9b97fa470a392b14126daf1
lauro <laurolyra@gmail.com>
2020-02-01 16:55:45 -0300

    API do Waze finalizada

--- src/components/Waze.js
+++ src/components/Waze.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
-export default class Waze extends React.Component{
+export default class Waze extends React.Component {
   render() {
-    return(
+    return (
       <div>
-        <iframe className = 'waze-API' src="https://embed.waze.com/pt-BR/iframe?zoom=12&lat=19.8157&lon=-43.9542 19"
-  width="300" height="400"></iframe>
+        <h2>Informações do Waze:</h2>
+        <iframe className='waze-API' src="https://embed.waze.com/pt-BR/iframe?&zoom=15&lat=-19.953266&lon=-44.013061&pin=1" width="350" height="350"></iframe>
+        <p>(para conferir mais informações sobre o trânsito, clique em "exibir mapa maior")</p>
       </div>
     )
   };
