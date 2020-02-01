d7b676c62c60efd0c56ea4c13c07ed0be7fff88e
Mateus Talles <mateustalles@gmail.com>
2020-02-01 19:54:19 -0300

    Mudança de tamanho

--- src/components/TwitterContainer.js
+++ src/components/TwitterContainer.js
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { Component } from "react";
 import { TwitterTimelineEmbed } from "react-twitter-embed";
 
-const TwitterContainer = () => {
-  return (
-    <section className="twitterContainer">
-      <div className="twitter-embed">
-        <TwitterTimelineEmbed
-          sourceType="list"
-          id="1223715354016452609"
-          options={{
-            tweetLimit: "100",
-            width: "350px",
-            height: "350px",
-          }}
-          // cards = "hidden"
-          // theme="dark"
-          // noHeader= {true}
-          // noBorders = {true}
-          // noFooter = {true}
-        ></TwitterTimelineEmbed>
-      </div>
-    </section>
-  );
+class TwitterContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      story: 3,
+    };
+  }
+  render() {
+    const { story } = this.state;
+    return (
+      <section className="twitterContainer">
+        <div className="twitter-embed">
+          <TwitterTimelineEmbed
+            sourceType="list"
+            id="1223715354016452609"
+            options={{
+              tweetLimit: {story},
+              width: "257px",
+              height: "160px",
+            }}
+            cards="hidden"
+            theme="dark"
+            noHeader={true}
+            noBorders={true}
+            noFooter={true}
+          ></TwitterTimelineEmbed>
+        </div>
+      </section>
+    );
+  }
 };
 
 export default TwitterContainer;
--- src/components/Waze.js
+++ src/components/Waze.js
@@ -5,7 +5,7 @@ export default class Waze extends React.Component {
     return (
       <div>
         <h2>Informações do Waze:</h2>
-        <iframe title="wazeMbeded" className='waze-API' src="https://embed.waze.com/pt-BR/iframe?&zoom=15&lat=-19.953266&lon=-44.013061&pin=1" width="350" height="350"></iframe>
+        <iframe title="wazeMbeded" className='waze-API' src="https://embed.waze.com/pt-BR/iframe?&zoom=15&lat=-19.953266&lon=-44.013061&pin=1" width="257" height="257"></iframe>
         <p>(para conferir mais informações sobre o trânsito, clique em "exibir mapa maior")</p>
       </div>
     )
