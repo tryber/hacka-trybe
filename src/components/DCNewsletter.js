5ebfcb35b44c17c8dd4e26716f123cf2625bd18b
Mateus Talles <mateustalles@gmail.com>
2020-02-01 14:45:26 -0300

    DCNewsletter

---
+++ src/components/DCNewsletter.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './DCNewsletter.css';
+
+function DCNewsletter() {
+  return (
+    <div className="DCNewsletter">
+      <header className="DCNewsletter-header">
+        <p>
+          Insira aqui os componentes
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default DCNewsletter;
