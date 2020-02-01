c39ce6c068c37878e3af15cea01290a0fe96751e
Mateus Talles <mateustalles@gmail.com>
2020-02-01 15:34:21 -0300

    Instalado rp, cheerio e cli-table

--- src/components/DCNewsletter.js
+++ src/components/DCNewsletter.js
@@ -1,6 +1,25 @@
 import React from 'react';
 import './DCNewsletter.css';
 
+const rp = require('request-promise');
+const cherio = require('cheerio');
+const Table = require('cli-table');
+
+let users = [];
+const options = {
+  url: 'https://prefeitura.pbh.gov.br/noticias/defesa-civil',
+  json: true,
+}
+
+rp(options)
+  .then((data) => {
+    let promises = [];
+    let usedData = [];
+    for (let user of data.directory_items) {
+      userData.push({})
+    }
+  })
+
 function DCNewsletter() {
   return (
     <div className="DCNewsletter">
