de890ab22ec05de9715a06b11be78fa573ac0d12
richardwellerson <richardwellerson@gmail.com>
2020-02-01 18:22:37 -0300

    Terminado projeto

--- src/components/inputSearch.jsx
+++ src/components/inputSearch.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
 class InputSearch extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      userAddress: '',
+    }
+  }
+  sendAPIAddress() {
+    console.log(`?s=${this.state.userAddress}`);
+    return `?s=${this.state.userAddress}`;
+  }
+
   render() {
     return (
       <div>
         <input
           placeholder="Ex: Rua Um 131 Belo Horizonte MG"
+          onChange={(e) => this.setState({ userAddress: e.target.value })}
         />
-        <button>
-          Clique Aqui man!
-        </button>
+        <button onClick={() => this.sendAPIAddress()}>Pesquisar</button>
       </div>
     );
   }
