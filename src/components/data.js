2d6bafd1a55e22352f60550f2cdccebcf08558d8
Douglas Henrique <doughrqe@gmail.com>
2020-02-01 17:42:53 -0300

    Introdução de conteúdo[

--- src/components/data.js
+++ src/components/data.js
@@ -2,20 +2,65 @@ const Postagens = [
     {
         header: {
             title: 'Dicas',
-            data: 'teste',
+            data: '01/02/2020',
             category: 'Dicas',
         },
-        text: 'teste',
-        image: 'https://img.estadao.com.br/thumbs/640/resources/jpg/4/6/1568205663464.jpg',
+        text: 'Lixo, coloque em áreas não alagáveis, que não obstrua a passagem da água. Sinais de Risco: Rachadaduras nos terrenos, muros e paredes Inclinação de postes, árvores ou muros embarrigados, Estalos em blocos de rocha ou aumento das trincas no solo ou paredes, Água mais barrenta que de costume, Morros com o acúmulo de água são causas de deslisamento, Lugares proximos a rio e de baixa altítude são áreas de risco , podem ter ruas construídas em cima de rios',
+        image: 'https://svgsilh.com/svg/1968595-009688.svg',
+    },
+    {
+        header: {
+            title: 'Durante a chuva e inundação:',
+            data: '01/02/2020',
+            category: 'O que fazer?',
+        },
+        text: 'Evite o contato com direto com a água, fique atentos crianças, Veja se há rachaduras ou risco da casa desabar,Procure um lugar seguro e evite se deslocar, Ao atravessar ruas alagadas você pode ser arrastado ou contaminado, Preste atenção em buracos e bueiros, Use calçados e roupas para proteger seu corpo, Nossas vidas são prioridade, se proteja e cuide de quem está envolta, Se estiver de carro, diminua a velocidade, matenha distacia do veículo à frente. Pare em lugares altos. Se o nível de água estiver subindo, vá com seus companheiros para um lugar seguro',
+        image: 'https://s2.glbimg.com/dZyiUOMELL6L6mIyYflsa5aQWzM=/0x0:1280x720/1008x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/M/S/QWGlsBTTADRjZpWbfraw/raposos-enchente.jpg',
+    },
+    {
+        header: {
+            title: 'Causas DE ALAGAMENTO:',
+            data: '01/02/2020',
+            category: 'Causas',
+        },
+        text: 'Meteorológicas/Atmosféricas: chuva intensas, derretimento da neve, tufão, furacões, ciclones Antropológicas: Mau uso e planejamento do meio urbano. Impermeabilidade do solo',
+        image: 'https://www.otempo.com.br/image/contentid/policy:1.2287701:1579741957/WhatsApp%20Image%202020-01-20%20at%2017.31.32.jpeg?f=3x2&w=620&$p$f$w=d3501d9',
+    },
+    {
+        header: {
+            title: 'Prevenção',
+            data: '01/02/2020',
+            category: 'Prevenção',
+        },
+        text: 'PREVENSÃO: Em regiões sujeitas a enchentes ou delisamento, se previna quando houver ameaça de chuva forte, Coloque moveis em lugar altos, Deslique a chave elétrica e registro de gás e água, Abrigue-se onde a água não o alcance, Em caso de risco, saia de casa, procure lugar seguro e ligue 199, para defesa Civil, Procure se a região é propensa a inundação, compartilhe, Limpe telhado, calhas, para não acumular água e ceder. Coloque documentos e objetos de valor em sacos plásticos bem fechados e em local protegido. Seja solidário, avise seus vizinhos sobre o risco',
+        image: 'https://s2.glbimg.com/MqfSSv3oPu70pi5ZgeKZZHWnneU=/0x0:1920x1080/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2020/F/a/OHeFcnQJSbp5A3o2gz1w/areas-de-chuva-em-bh.jpeg',
+    },
+    {
+        header: {
+            title: 'Quem tem direito a receber o Aluguel Social',
+            data: '01/02/2020',
+            category: 'Juridico',
+        },
+        text: 'Apesar de os acidentes com moradias serem muito traumáticos e gerarem bastante gasto, não são todas as vítimas disso que podem solicitar ao Governo o Aluguel Social. Existem alguns perfis de famílias para os quais esse benefício é permitido, como as que residem em áreas de risco e as que estão no Tarifa Social de Energia Elétrica. O auxílio-aluguel pode ser dado a quem está no Bolsa Família e àqueles que passaram por desastres naturais, além dos incluídos no Cadastro Único. Destaca-se que o Programa de Aceleração do Crescimento (PAC) também foi razão para que várias pessoas recebessem o Salário Aluguel, uma vez que tiveram de sair de suas residências. Sendo assim, os brasileiros podem requisitar esse auxílio se alguma obra prejudicar o seu imóvel.',
+        image: 'https://www.assistenciasocial.org/wp-content/uploads/2018/09/quem-direito-receber-aluguel-social-1.jpg',
     },
     {
         header: {
             title: 'teste',
-            data: 'teste',
-            category: 'teste',
+            data: '01/02/2020',
+            category: 'Ajudar',
         },
-        text: 'teste',
-        image: 'https://img.estadao.com.br/thumbs/640/resources/jpg/4/6/1568205663464.jpg',
+        text: 'O Serviço Social Autônomo (Servas) lançou campanha de arrecadação na quinta-feira (23). Até então, eram atendidos pedidos pontuais, de acordo com as necessidades dos municípios. Mas, com o agravamento da situação após as tempestades, foi iniciada uma nova ação. “Iniciamos a campanha na tarde da quinta-feira pelas redes sociais e esperamos um aumento das doações. Para isso, estaremos funcionando no sábado e também no domingo das 8h às 12h”, explica a técnica social Sarah Prado.Para contribuir, basta entregar os materiais na sede da entidade, na avenida Cristóvão Colombo, 683, próximo ao Palácio da Liberdade, no bairro Funcionários, região Centro-Sul de BH. A Cruz Vermelha também lançou uma ação na quarta-feira (22). O volume dos donativos, porém, está abaixo da demanda. “As doações estão chegando, mas não têm sido suficientes para atender as regiões afetadas. Enviamos há pouco um caminhão para auxiliar as vítimas de Raposos (Grande BH). Ainda não sabemos qual é a necessidade por lá, mas acreditamos que a demanda será maior. Por isso, vamos reforçar nossa campanha”, diz o voluntário Artênius Daniel. Onde doar Servas Avenida Cristóvão Colombo, 683 (próximo ao Palácio da Liberdade), no bairro Funcionários',
+        image: 'https://www.hojeemdia.com.br/polopoly_fs/1.209521.1579892143!/image/image.jpg_gen/derivatives/landscape_653/image.jpg',
+    },
+    {
+        header: {
+            title: 'DEPOIS DA CHUVA:',
+            data: '01/02/2020',
+            category: 'Outros',
+        },
+        text: 'Beba apenas água filtrada ou fervida, Não use alimentos que estavam em contato com a água, Fique atento a sintomas como: febre, vômito, dor de cabeça ou no corpo; no caso procure serviços de saúde, essa água pode trazer várias doenças, Chuvas de grande intensidade ou longa duração provocam deslizamentos: fique atento as áreas de risco e a todo sinal, Não use equipamentos elétricos molhados ou que estiveram na zona de inundação, Lave e desinfete os objetos atingidos pela enchente usando uma mistura de um copo de água sanitária para cada balde de 20 litros de água limpa, utilizando luvas e botas.',
+        image: 'https://lh6.googleusercontent.com/proxy/YSj2VTW-k-5eLhZdBWL2P_VfLSkhKxwHs5JRx-Hof3oJn6zTYoL_zccb95QOdzk2d2nMAKZzjkzo-vBikYd3pArwtw0SNPsxQRmYAo2uZoH11t4uzDB3072RsWqO_ZcYr7H8eIU',
     }
 ]
 export default Postagens;
