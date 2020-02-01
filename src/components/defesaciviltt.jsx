90df54bda5ba3cdc9a4314d9f7a3c519ff5f3db1
Giorgia <giorgialeoniac@gmail.com>
2020-02-01 15:34:41 -0300

    Envio para Matheus Conferir

--- src/components/defesaciviltt.jsx
+++ src/components/defesaciviltt.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 class DefesaCivil extends React.Component {
     render() {
         return (
-            <div>
-            <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/defesacivilbh?ref_src=twsrc%5Etfw">Tweets by defesacivilbh</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            <div className="feed-defesa-civil-bh">
+                <a className="twitter-timeline" data-theme="dark" href="https://twitter.com/defesacivilbh?ref_src=twsrc%5Etfw">Tweets by defesacivilbh</a> <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
             </div>
         );
     }
 }
 
-export default defesaCivil;
+export default DefesaCivil;
