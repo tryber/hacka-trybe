0f8cad9a9fb2fcd3c93ad628c2439e24eeb4cc32
Giorgia <giorgialeoniac@gmail.com>
2020-02-01 15:04:48 -0300

    Envio do componente de Twitter da Defesa Civil

---
+++ src/components/defesaciviltt.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class DefesaCivil extends React.Component {
+    render() {
+        return (
+            <div>
+            <a class="twitter-timeline" data-theme="dark" href="https://twitter.com/defesacivilbh?ref_src=twsrc%5Etfw">Tweets by defesacivilbh</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+            </div>
+        );
+    }
+}
+
+export default defesaCivil;
