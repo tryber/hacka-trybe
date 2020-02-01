b0a5ccb91755eb33c03828340981f69b2e881834
antoniopaolillo <antonio_francisco_p@live.com>
2020-02-01 19:23:45 -0300

    filtrando via geoLocalização

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { Provider } from './components/context/MapContext';
 import MapInputs from './components/MapInputs';
+import Geolocation from './components/Geolocation';
 
 function App() {
   return (
     <div className="App">
       <Provider>
         <MapInputs />
+        <Geolocation />
       </Provider>
     </div>
   );
--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -1,38 +1,70 @@
 import React from 'react';
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { MapContext } from './context/MapContext';
 
 export class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    };
+  }
+ 
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+ 
+  onMapClicked = (props) => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      })
+    }
+  }
   render() {
+    const { geolocation } = this.context;
     return (
       <div>
         <Map
           initialCenter={{
-            lat: -19.9412,
-            lng: -43.93128,
+            lat: geolocation.latitude,
+            lng: geolocation.longitude,
           }}
           google={this.props.google}
           zoom={14}
+          onClick={this.onMapClicked}
         >
           {this.props.data.map((point) => {
             return (
               <Marker
+                onClick={this.onMarkerClick}
                 key={point.address}
                 title={point.address}
+                name={point.address}
                 position={{ lat: point.latitude, lng: point.longitude }}
               />
             );
           })}
-          <InfoWindow onClose={this.onInfoWindowClose}>
+          <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}>
             <div>
-              <h1>top</h1>
+              <h1>{this.state.selectedPlace.name}</h1>
             </div>
-          </InfoWindow>
+        </InfoWindow>
         </Map>
       </div>
     );
   }
 }
 
+MapContainer.contextType = MapContext;
 export default GoogleApiWrapper({
   apiKey: 'AIzaSyDg2CCtZwxt0DZXmOtT2rK4oBKzUNkfGok',
 })(MapContainer);
