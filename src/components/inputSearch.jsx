8b9b550ada063f42712665a7a53032892831d0da
richardwellerson <richardwellerson@gmail.com>
2020-02-01 17:38:14 -0300

    Input criado

---
+++ src/components/inputSearch.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class InputSearch extends React.Component {
+  render() {
+    return (
+      <div>
+        <input
+          placeholder="Ex: Rua Um 131 Belo Horizonte MG"
+        />
+        <button>
+          Clique Aqui man!
+        </button>
+      </div>
+    );
+  }
+}
+
+export default InputSearch;
