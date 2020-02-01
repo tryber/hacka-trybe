12cfd09552dedf8571f3a34687bc46a12ecf7134
Mateus Talles <mateustalles@gmail.com>
2020-02-01 14:14:50 -0300

    First commit

---
+++ src/components/Alerta.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Alerta.css';
+
+function Alerta() {
+  return (
+    <div className="Alerta">
+      <header className="Alerta-header">
+        <p>
+          Insira aqui os componentes
+        </p>
+       
+      </header>
+    </div>
+  );
+}
+
+export default Alerta;
