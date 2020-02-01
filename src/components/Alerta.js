aea59d977097d4c3c18a5cefdf39cbada17afa36
lauro <laurolyra@gmail.com>
2020-02-01 18:30:52 -0300

    inclusão de Waze na feature alerta

--- src/components/Alerta.js
+++ src/components/Alerta.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Alerta.css';
 import TwitterContainer from './TwitterContainer';
+import Waze from './Waze'
 
 function Alerta() {
   return (
     <div className="Alerta">
       <header className="Alerta-header">
        <TwitterContainer />
-       
+       <Waze />       
       </header>
     </div>
   );
