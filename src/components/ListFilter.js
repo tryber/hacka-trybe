cb816738b33a89dade4665b74baed3bb724999b3
Henrique Eyer <henriqueeyer@outlook.com>
2020-02-01 16:13:31 -0300

    Terminando os filtros

---
+++ src/components/ListFilter.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Filter from './Filter.js'
+
+class ListFilter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: ['Dicas', 'O que fazer?', 'Causas', 'Prevenção', 'Juridico', 'Ajudar', 'Outros'],
+    };
+  }
+
+  render() {
+    const { changeSelectedFilter, select } = this.props;
+    const { categories } = this.state;
+    console.log(categories)
+    return (
+      categories.map((category) => <Filter name={category} change={changeSelectedFilter} select={select} />)
+    );
+  }
+}
+
+export default ListFilter;
