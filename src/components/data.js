f20e2b3f811e7c12d1a216afdee0872bbe0a7f23
Mateus Talles <mateustalles@gmail.com>
2020-02-01 23:50:14 -0300

    about page with pictures

--- src/components/data.js
+++ src/components/data.js
@@ -1,17 +1,19 @@
 const participantes = {
-    'Diego Augusto': 'https://www.linkedin.com/in/diegoaugust/',
-    'Antonio Paolillo': 'https://www.linkedin.com/in/antoniopaolillo/',
-    'Guilherme Crespo': 'https://www.linkedin.com/in/guicrespo/',
-    'Douglas Henrique': 'https://www.linkedin.com/in/douglas-he/',
-    'Giorgia Leonia': 'https://www.linkedin.com/in/giorgialeonia/',
-    'Richard Wellerson': 'https://www.linkedin.com/in/richardwellerson/',
-    'Guilherme Luiz Campos': 'https://www.linkedin.com/in/guilherme-assis-campos/',
-    'Renato Álvares': 'https://www.linkedin.com/in/renato-%C3%A1lvares-7575011a1/',
-    'Pedro Henrique Tófani': 'https://www.linkedin.com/in/pedro-henrique-t%C3%B3fani-ferreira-110954109/',
-    'Lauro Lyra': 'https://www.linkedin.com/in/laurolyra',
-    'Miguel Miranda': '',
-    'Johnatas Henrique': 'https://www.linkedin.com/in/johnatashenrique/',
-    'Andrey Kenji Tsuzuki': 'https://www.linkedin.com/in/andrey-tsuzuki/'
+    'Diego Augusto': ['https://www.linkedin.com/in/diegoaugusto/', 'https://media-exp1.licdn.com/dms/image/C4E03AQGhovf02kT3RA/profile-displayphoto-shrink_800_800/0?e=1586390400&v=beta&t=3wSUlCAWllU3gwWmFp9QOD7WThG119YVxlZoIZgogxo'],
+    'Antonio Paolillo': ['https://www.linkedin.com/in/antoniopaolillo/', 'https://media-exp1.licdn.com/dms/image/C4E03AQFs8YDvA-lULw/profile-displayphoto-shrink_800_800/0?e=1586390400&v=beta&t=5om0HnZIWUsFQVhJ7eaYxlwmKnnVuZrbOleqU0U2EJM'],
+    'Guilherme Crespo': ['https://www.linkedin.com/in/guicrespo/', 'https://ca.slack-edge.com/TMDDFEPFU-UPL732W11-43d623c27338-512'],
+    'Douglas Henrique': ['https://www.linkedin.com/in/douglas-he/', 'https://media-exp1.licdn.com/dms/image/C4E03AQFKaihegMxIpA/profile-displayphoto-shrink_800_800/0?e=1586390400&v=beta&t=IGBLgsWSSuy8rHJkIcAskwiOvpykvUbroljkkCTWPz8'],
+    'Giorgia Leonia': ['https://www.linkedin.com/in/giorgialeonia/', 'https://media-exp1.licdn.com/dms/image/C4E03AQE1Rr5MPT_yqA/profile-displayphoto-shrink_800_800/0?e=1586390400&v=beta&t=0DQQh55VJUEoIRjU4tJEpnxUvXqEcAi43T5ZoAWjalI'],
+    'Richard Wellerson': ['https://www.linkedin.com/in/richardwellerson/', 'https://media-exp1.licdn.com/dms/image/C4E03AQHKXLQRoUewuw/profile-displayphoto-shrink_800_800/0?e=1586390400&v=beta&t=Dw72H_GI8dAl3CHUkjF-ILnilHfLo3GwNX0vN-ULqes'],
+    'Guilherme Luiz Campos': ['https://www.linkedin.com/in/guilherme-assis-campos/', 'https://media-exp1.licdn.com/dms/image/C4D03AQEYtME6dh43gg/profile-displayphoto-shrink_800_800/0?e=1586390400&v=beta&t=aOFWouOi2cmk3c9k9CL3V4s99zQJ6QGbLJCopEWJVek'],
+    'Renato Álvares': ['https://www.linkedin.com/in/renato-%C3%A1lvares-7575011a1/', 'https://ca.slack-edge.com/TMDDFEPFU-UT6AJSVRT-d69635eec68d-512'],
+    'Pedro Henrique Tófani': ['https://www.linkedin.com/in/pedro-henrique-t%C3%B3fani-ferreira-110954109/','https://media-exp1.licdn.com/dms/image/C5603AQFg4MNW6WWYVA/profile-displayphoto-shrink_800_800/0?e=1586390400&v=beta&t=Iqefrf3qbYzR9n5BJEXQ3cxOakvQjrzdWdFNCDKDo0c'],
+    'Lauro Lyra': ['https://www.linkedin.com/in/laurolyra', 'https://media-exp1.licdn.com/dms/image/C4E03AQEv3kNvRTJgig/profile-displayphoto-shrink_800_800/0?e=1586390400&v=beta&t=coJ_jKm2iLeeAw19RMvA96VoNIjslQNwqJZFn6mUc6U'],
+    'Miguel Miranda': ['','https://ca.slack-edge.com/TMDDFEPFU-UPJTJA2HG-e9a292fa3f29-512'],
+    'Johnatas Henrique': ['https://www.linkedin.com/in/johnatashenrique/', 'https://ca.slack-edge.com/TMDDFEPFU-UPPQ6LEKB-df8024b3f24a-512'],
+    'Andrey Kenji Tsuzuki': ['https://www.linkedin.com/in/andrey-tsuzuki/', 'https://ca.slack-edge.com/TMDDFEPFU-UPXN7AP96-e60d3769a8d7-512'],
+    'Douglas "Funny" Eduardo': ['https://www.linkedin.com/in/dougfunny/','https://media-exp1.licdn.com/dms/image/C4E03AQGNTYa259De4Q/profile-displayphoto-shrink_800_800/0?e=1586390400&v=beta&t=iBEwjwn0HVbwCYhw6S3d29ycRKbiy6Lw_ES_IHRdiDM'],
+    'Mateus Talles Carvalho': ['https://www.linkedin.com/in/mateustalles/', 'https://media-exp1.licdn.com/dms/image/C4E03AQHWvDYwqkPbJg/profile-displayphoto-shrink_800_800/0?e=1586390400&v=beta&t=pzH5adSF7BmdZX_D0viAnwmcqzmJP_NOmwnlTBuI0Uk'],
 }
 
 export default participantes;
--- src/components/nomes.js
+++ src/components/nomes.js
@@ -1,17 +1,29 @@
 import React from 'react';
 import nomes from './data';
+import './nomes.css'
 
 function ListarNome(props) {
-return <li><a href={props.pessoaLinkedin} target='_blank' rel="noopener noreferrer">{props.nomeDaPessoa}</a></li>
+return (
+    <li>
+        <a href={props.pessoaLinkedin} target='_blank' rel="noopener noreferrer">
+            <div className="tryber">
+                <img src={props.imgSrc} alt={props.key} />
+                {props.nomeDaPessoa}
+            </div>
+        </a>
+    </li>
+);
 }
 
 class NomesLista extends React.Component {
     render() {
         const objNomes = nomes;
         const arrNomes = Object.keys(objNomes)
-        return (<ul>
-            {arrNomes.map((pessoa) => <ListarNome key={pessoa} pessoaLinkedin={nomes[pessoa]} nomeDaPessoa={pessoa} />)}
-        </ul>);
+        return (
+            <ul>
+                {arrNomes.map((pessoa) => <ListarNome key={pessoa} imgSrc={objNomes[pessoa][1]} pessoaLinkedin={objNomes[pessoa][0]} nomeDaPessoa={pessoa} />)}
+            </ul>
+        );
     }
 }
 export default NomesLista;
