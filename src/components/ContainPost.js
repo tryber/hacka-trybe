b420134f2acc2fe414304518889c7598a4fa1dec
Douglas Henrique <doughrqe@gmail.com>
2020-02-01 21:02:22 -0300

    Alteração pra api

--- src/components/ContainPost.js
+++ src/components/ContainPost.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import Post from './Post';
-import postagens from './data';
+import * as ApiPost from './ApiPost';
 
 class ContainPost extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            data: [],
+            success: false,
+        }
+    }
+
+    componentDidMount() {
+        ApiPost.getPost()
+            .then((data) => {
+                console.log(data)
+                return this.setState({
+                    data,
+                    success: true,
+                })
+            });
+    }
     render() {
         const { select } = this.props;
-        const filterPost = (select) ? postagens.filter((post) => post.header.category === select) : postagens;
-        if(filterPost.length===0)return<div>Nenhum Resultado Encontrado</div>
+        const { data, success } = this.state;
+        if (!success) return <div>Carregando!</div>
+        const dataSorted = data.sort(function (a, b) { return Date.parse(b.date) - Date.parse(a.date) });
+        console.log(dataSorted)
+        const filterPost = (select) ? data.filter((post) => post.category === select) : data;
+        if (filterPost.length === 0) return <div>Nenhum Resultado Encontrado</div>
         return (
             filterPost.map(posts => <Post itemPost={posts} />)
         );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 class Header extends React.Component {
   render() {
-    const { title, data, category } = this.props.headers;
+    const { title, date, category } = this.props;
     return (
       <div>
         <h4>{title}</h4>
-        <h6>{data}</h6>
+        <h6>{date}</h6>
         <h6>{category}</h6>
       </div>
     )
