d88cdfc2672a7611eafc2ba2392646b7c714afbd
richardwellerson <richardwellerson@gmail.com>
2020-02-02 19:55:45 -0300

    Inserindo Link correto

--- src/components/data.js
+++ src/components/data.js
@@ -4,7 +4,7 @@ const participantes = {
     'Guilherme Crespo': ['https://www.linkedin.com/in/guicrespo/', 'https://ca.slack-edge.com/TMDDFEPFU-UPL732W11-43d623c27338-512'],
     'Douglas Henrique': ['https://www.linkedin.com/in/douglas-he/', 'https://media-exp1.licdn.com/dms/image/C4E03AQFKaihegMxIpA/profile-displayphoto-shrink_800_800/0?e=1586390400&v=beta&t=IGBLgsWSSuy8rHJkIcAskwiOvpykvUbroljkkCTWPz8'],
     'Giorgia Leonia': ['https://www.linkedin.com/in/giorgialeonia/', 'https://media-exp1.licdn.com/dms/image/C4E03AQE1Rr5MPT_yqA/profile-displayphoto-shrink_800_800/0?e=1586390400&v=beta&t=0DQQh55VJUEoIRjU4tJEpnxUvXqEcAi43T5ZoAWjalI'],
-    'Richard Wellerson': ['https://www.linkedin.com/in/richardwellerson/', 'https://i.imgur.com/copWOrT.png'],
+    'Richard Wellerson': ['https://www.linkedin.com/in/richardwellerson/', 'https://media-exp1.licdn.com/dms/image/C4E03AQGwtKOnzfI3Ww/profile-displayphoto-shrink_200_200/0?e=1586390400&v=beta&t=Em0kEVq13TPAofXkXu43mZH7nC5nyM99VeMbTEU_eu4'],
     'Guilherme Luiz Campos': ['https://www.linkedin.com/in/guilherme-assis-campos/', 'https://media-exp1.licdn.com/dms/image/C4D03AQEYtME6dh43gg/profile-displayphoto-shrink_800_800/0?e=1586390400&v=beta&t=aOFWouOi2cmk3c9k9CL3V4s99zQJ6QGbLJCopEWJVek'],
     'Renato Álvares': ['https://www.linkedin.com/in/renato-%C3%A1lvares-7575011a1/', 'https://ca.slack-edge.com/TMDDFEPFU-UT6AJSVRT-d69635eec68d-512'],
     'Pedro Henrique Tófani': ['https://www.linkedin.com/in/pedro-henrique-t%C3%B3fani-ferreira-110954109/', 'https://media-exp1.licdn.com/dms/image/C5603AQFg4MNW6WWYVA/profile-displayphoto-shrink_800_800/0?e=1586390400&v=beta&t=Iqefrf3qbYzR9n5BJEXQ3cxOakvQjrzdWdFNCDKDo0c'],
