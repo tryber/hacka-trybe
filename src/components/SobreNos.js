b9d61a922394b5422bcca1aa1f621d838a8d1188
richardwellerson <richardwellerson@gmail.com>
2020-02-02 20:54:00 -0300

    Atualizaçoes do texto e inserçao do Link

--- src/components/SobreNos.js
+++ src/components/SobreNos.js
@@ -1,14 +1,18 @@
 import React from 'react';
-import ListaSobreNos from './nomes'
-import './SobreNos.css'
+import { Link } from 'react-router-dom';
+import ListaSobreNos from './nomes';
+import './SobreNos.css';
 
 function App() {
   return (
     <div className="about">
+      <Link to="/">
+        <button className="close-about"></button>
+      </Link>
       <h1>Quem somos nós</h1>
       <br />
       <br />
-      <p>Em um sábado, 1º de fevereiro, nos foi dado um desafio: o que pode ser feito, em apenas um dia, com nossas habilidades e tempo, para mitigar os impactos das chuvas que recentemente pôs em risco a vida de muitos e foram fatais para outros.
+      <p>No dia 1º de fevereiro, nos foi dado um desafio: o que pode ser feito, em apenas um final de semana, com nossas habilidades e tempo, para mitigar os impactos das chuvas que recentemente pôs em risco a vida de muitos e foram fatais para outros.
 Há apenas poucos meses, começamos a estudar programação: com aprendizagem ativa, esforço diário, organização e colaboração. Foi assim que saímos da ideia de um desafio, e transformamos esse desconforto em material: um centro de informações para que qualquer pessoa utilizasse como bússola e pudesse se orientar em meio à confusão.
 Graças à nossa união e à orientação da equipe da Trybe, conseguimos transfomar essa ideia em realidade.</p>
       <p>Somos Trybers!</p>
