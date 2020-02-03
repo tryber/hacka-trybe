c2eca8851a2067c4c5c912aa42d024583842d00f
Alberto de Sá Cavalcanti de Albuquerque <albertoalbuquerque01@gmail.com>
2020-02-02 21:51:50 -0300

    Remove unecessary img styling and fix identation

--- src/components/nomes.js
+++ src/components/nomes.js
@@ -1,33 +1,44 @@
 import React from 'react';
 import nomes from './data';
 import linkedinLogo from './Group 62.svg';
-import './nomes.css'
+import './nomes.css';
 
 function ListarNome(props) {
-    return (
-        <>
-            <a className="tryber" href={props.pessoaLinkedin} target='_blank' rel="noopener noreferrer">
-                <img className="trybersImg" src={props.imgSrc} alt={props.key} />
-                <br/>
-                <div>
-                    <span className="trybe-name" >{props.nomeDaPessoa}</span>
-                </div>
-            </a>
-        </>
-    );
+  return (
+    <>
+      <a
+        className="tryber"
+        href={props.pessoaLinkedin}
+        target="_blank"
+        rel="noopener noreferrer">
+        <img className="trybersImg" src={props.imgSrc} alt={props.key} />
+        <br />
+        <div>
+          <span className="trybe-name">{props.nomeDaPessoa}</span>
+        </div>
+      </a>
+    </>
+  );
 }
 
 class NomesLista extends React.Component {
-    render() {
-        const objNomes = nomes;
-        const arrNomes = Object.keys(objNomes)
-        return (
-            <>
-                <div className="trybers-list">
-                    {arrNomes.map((pessoa) => <ListarNome key={pessoa} imgSrc={objNomes[pessoa][1]} pessoaLinkedin={objNomes[pessoa][0]} nomeDaPessoa={pessoa} />)}
-                </div>
-            </>
-        );
-    }
+  render() {
+    const objNomes = nomes;
+    const arrNomes = Object.keys(objNomes);
+    return (
+      <>
+        <div className="trybers-list">
+          {arrNomes.map(pessoa => (
+            <ListarNome
+              key={pessoa}
+              imgSrc={objNomes[pessoa][1]}
+              pessoaLinkedin={objNomes[pessoa][0]}
+              nomeDaPessoa={pessoa}
+            />
+          ))}
+        </div>
+      </>
+    );
+  }
 }
 export default NomesLista;
